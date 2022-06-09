640429d56c788f62272df3de2e495d898d0607f2
jismin <antonyjismin17@gmail.com>
2022-06-08 23:00:02 -0700

    add

---
+++ listworks/list3.py
@@ -0,0 +1,45 @@
+#print element in the array
+
+arr=[10,11,12,13,14,15,16]
+
+# flag=0
+# element=12
+#
+# arr.sort()
+# low=0
+# upp=len(arr)-1
+# while(low<=upp):
+#     mid=(low+upp)//2
+#     if(element==arr[mid]):
+#         flag=1
+#         break
+#     elif (element>arr[mid]):
+#        low=mid+1
+#     elif (element<arr[mid]):
+#         upp=mid-1
+# print("found" if flag>0 else "not found")
+
+#print commom nos in array
+
+# arr1=[10,11,12,13,14,15]
+# arr2=[9,10,11,14,20,21]
+#
+# p1=0
+# p2=0
+# arr1.sort()
+# arr2.sort()
+# while(p1<len(arr1) and p2<len(arr2)):
+#     if arr1[p1]==arr2[p2]:
+#         print(f"common element {arr[p1]}")
+#         p1+=1
+#         p2+=1
+#     elif arr1[p1]>arr2[p2]:
+#         p2+=1
+#     elif arr1[p2]<arr2[p2]:
+#          p1+=1
+
+
+
+
+
+
